f2588736c29fe93a5f37800076a3948ca222c32b
Denis <45292534+denis-sp@users.noreply.github.com>
2021-11-15 01:07:38 +0300

    Add files via upload

--- hackrf/bld.bat
+++ hackrf/bld.bat
@@ -14,13 +14,20 @@ cmake -G "MinGW Makefiles" ^
     -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     -DLIB_INSTALL_DIR:PATH="lib" ^
     -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+    -DLIBUSB_INCLUDE_DIR:PATH="%LIBRARY_INC%\libusb-1.0" ^
+    -DLIBUSB_LIBRARIES:PATH="%LIBRARY_LIB%\libusb-1.0.lib" ^
+    -DFFTW_INCLUDES:PATH="%LIBRARY_INC%" ^
+    -DFFTW_LIBRARIES:PATH="%LIBRARY_LIB%\fftw3f.lib" ^
+    -DTHREADS_PTHREADS_WIN32_LIBRARY:FILEPATH="%LIBRARY_LIB%\pthread.lib" ^
     ..
 
 if errorlevel 1 exit 1
 
-mingw32-make
+cmake --build . --config Release
 if errorlevel 1 exit 1
 
-mingw32-make install
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
+
+del %LIBRARY_BIN%\hackrf_static.lib
 
